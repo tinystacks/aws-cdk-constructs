509df4749069099757bb4907073a6c6258c7e0a8
sivaroj limvipuwat <sivarojlimvipuwat@sivarojs-MacBook-Pro.local>
2022-09-19 16:14:41 -0700

    add cdk ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AlbStack } from './stacks/AlbStack'
@@ -6,5 +6,5 @@ import { CreateVpc } from './stacks/CreateVpc'
 
 const app = new cdk.App();
 
-new CreateVpc(app, 'createVpc', { env: {account: '759747741894', region: 'us-west-2'}})
-new AlbStack(app, "albStack", { env: {account: '759747741894', region: 'us-west-2'}})
+new CreateVpc(app, 'createVpc', { env: { account: '759747741894', region: 'us-west-2' } })
+new AlbStack(app, "albStack", { env: { account: '759747741894', region: 'us-west-2' } })
