552d5de7d1bb773fcf771481f6018b9d88c9397b
sivaroj limvipuwat <sivarojlimvipuwat@sivarojs-MacBook-Pro.local>
2022-10-16 23:43:15 -0700

    remove s3 stuff

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { S3Creation } from './stacks/s3Creation';
 
 const app = new cdk.App();
-
-new S3Creation(app, 'testS3', { env: { account: '759747741894', region: 'us-west-2'}, existingBucketArn: "arn:aws:s3:::existing-bucket-test-1"})
