7c644a15d98bb11bde7d045339479aa892c93244
sivaroj limvipuwat <sivarojlimvipuwat@sivarojs-MacBook-Pro.local>
2022-11-07 16:43:36 -0800

    run lint

--- src/constructs/compute/s3.ts
+++ src/constructs/compute/s3.ts
@@ -1,7 +1,7 @@
-import { Construct } from 'constructs'
-import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
-import * as s3 from 'aws-cdk-lib/aws-s3'
-import { BucketProps } from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { BucketProps } from 'aws-cdk-lib/aws-s3';
 
 interface S3Props {
   bucketProps?: BucketProps
@@ -9,29 +9,29 @@ interface S3Props {
 }
 
 export class S3 extends Construct {
-  private readonly bucket: s3.IBucket
-  private readonly id: string
-  private readonly bucketProps: BucketProps | undefined
+  private readonly bucket: s3.IBucket;
+  private readonly id: string;
+  private readonly bucketProps: BucketProps | undefined;
 
   constructor (scope: Construct, id: string, props: S3Props) {
-    super(scope, id)
+    super(scope, id);
 
     const {
       existingBucketArn,
       bucketProps
-    } = props
-    this.id = id
-    this.bucketProps = bucketProps
+    } = props;
+    this.id = id;
+    this.bucketProps = bucketProps;
 
     if (existingBucketArn !== undefined) {
-      this.bucket = s3.Bucket.fromBucketArn(this, `${id}-bucket`, existingBucketArn)
+      this.bucket = s3.Bucket.fromBucketArn(this, `${id}-bucket`, existingBucketArn);
     } else {
-      this.bucket = this.createBucket()
+      this.bucket = this.createBucket();
     }
   }
 
   private createBucket () {
-    return new s3.Bucket(this, `${this.id}-bucket`, this.bucketProps)
+    return new s3.Bucket(this, `${this.id}-bucket`, this.bucketProps);
   }
 
   public uploadSource (source: [s3Deployment.ISource], bucketKeyPrefix: string) {
@@ -40,14 +40,14 @@ export class S3 extends Construct {
       prune: false,
       destinationBucket: this.bucket,
       destinationKeyPrefix: bucketKeyPrefix
-    })
+    });
   }
 
   public get bucketArn (): string {
-    return this.bucket.bucketArn
+    return this.bucket.bucketArn;
   }
 
   public get bucketName (): string {
-    return this.bucket.bucketName
+    return this.bucket.bucketName;
   }
 }
