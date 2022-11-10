3a8b82769b8746e5cb813f73f3dea617ee1b31b0
nefertitirogers <nefertitirogers@gmail.com>
2022-11-09 19:09:50 -0800

    init commit

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import { Rds } from '/Users/nefertitirogers/Projects/tinystacks-aws-cdk-constructs/src/constructs/rds/postgres'
 
 const app = new cdk.App();
+
